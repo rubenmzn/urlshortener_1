9b4a642fef63285ff00fbb310ad3124389bc2d79
Loreto Matinero <55485084+Loretix@users.noreply.github.com>
2023-12-19 19:40:51 +0000

    openapi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("org.webjars:jquery:${Version.JQUERY}")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
 
     runtimeOnly("org.hsqldb:hsqldb")
--- app/src/main/kotlin/es/unizar/urlshortener/Application.kt
+++ app/src/main/kotlin/es/unizar/urlshortener/Application.kt
@@ -4,10 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-
 /**
  * The marker that makes this project a Spring Boot application.
  */
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableScheduling
 class Application
